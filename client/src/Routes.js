9c217cf18b25cccd0fc69046db516c2858ac78a6
FabianUlises <ulisesfab.car@outlook.com>
2023-04-24 11:08:57 -0700

    Created form component

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Components
 import App from './App.js';
+import Create from './components/create/Create.js';
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact Component={App} />
+                <Route path="/create" exact Component={Create} />
             </Routes>
         </BrowserRouter>
     );
---
+++ client/src/components/create/Create.js
@@ -0,0 +1,10 @@
+// Dependencies
+import React from 'react';
+
+const Create = () => {
+  return (
+    <div>Create</div>
+  )
+};
+
+export default Create;
