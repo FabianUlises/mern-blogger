f59a4068e0d77517080eee6fde3d2fc31d40d305
FabianUlises <ulisesfab.car@outlook.com>
2023-04-24 14:36:21 -0700

    Added route for single post

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Components
 import App from './App.js';
 import Create from './components/create/Create.js';
+import SinglePost from './components/singlepost/SinglePost.js';
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact Component={App} />
                 <Route path="/create" exact Component={Create} />
+                <Route path='/post/:slug' exact Component={SinglePost} />
             </Routes>
         </BrowserRouter>
     );
---
+++ client/src/components/singlepost/SinglePost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SinglePost = () => {
+    console.log('single component');
+    return (
+        <div>SinglePost</div>
+    )
+};
+
+export default SinglePost;
