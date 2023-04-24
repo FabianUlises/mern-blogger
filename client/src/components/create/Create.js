1d62a475b21bb1ef2a9b0189c4080c0f8ace2db3
FabianUlises <ulisesfab.car@outlook.com>
2023-04-24 11:18:18 -0700

    Added content to form

--- client/src/components/create/Create.js
+++ client/src/components/create/Create.js
@@ -3,7 +3,25 @@ import React from 'react';
 
 const Create = () => {
   return (
-    <div>Create</div>
+    <div className='form-container'>
+        <form className='page-form'>
+            <div className='form-group'>
+                <label for='postTitle' className='form-label'>Title</label>
+                <input type='text' placeholder='Post title' required className='form-input' id='postTitle' />
+            </div>
+            <div className='form-group'>
+                <label for='postContent' className='form-label'>Content</label>
+                <textarea type='text' placeholder='Post content' required className='form-input' id='postContent' />
+            </div>
+            <div className='form-group'>
+                <label for='postUser' className='form-label'>User</label>
+                <input type='text' placeholder='Username' required className='form-input' id='postUser' />
+            </div>
+            <div className='form-btn-container'>
+                <button type='submit' className='form-btn-submit'>Submit</button>
+            </div>
+        </form>
+    </div>
   )
 };
 
