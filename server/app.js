af69c20f963618490220e6ed5f346d0e1cc51ad0
FabianUlises <ulisesfab.car@outlook.com>
2023-04-24 05:57:54 -0700

    Connected server to local db

--- server/app.js
+++ server/app.js
@@ -1,7 +1,33 @@
 // Dependencies
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+// Mongoose connection
+mongoose.connect(process.env.LOCAL_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((conn) => {
+    console.log(`Connected to db: ${process.env.LOCAL_DB}`);
+}).catch((err) => {
+    console.error('Error: ', err);
+});
+// Root route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'Success',
+        message: '/ root route',
+        data: 'root get route'
+    });
+});
+// Catch all route
+app.get('*', (req, res) => {
+    res.status(200).json({
+        status: 'Success',
+        message: 'Catch all route',
+        data: null
+    });
+});
 // Server on
 app.listen(
     process.env.PORT, 
