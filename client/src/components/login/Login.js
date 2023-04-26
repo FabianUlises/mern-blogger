53381962833347b344aeac09501cd6dcc245a8b7
FabianUlises <ulisesfab.car@outlook.com>
2023-04-25 17:19:42 -0700

    Added state to login component to handle user login info

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,10 +1,34 @@
 // Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    // State
+    const [user, setUser] = useState({
+        email: '',
+        name: '',
+        password: ''
+    });
+    return (
+        <div>
+            <form className='page-form'>
+                <div className='form-group'>
+                    <label for='userEmail' className='form-label'>Email</label>
+                    <input type='email' placeholder='your email' required className='form-input' id='userEmail' onChange={(e) => setUser({...user, email: e.target.value})} />
+                </div>
+                <div className='form-group'>
+                    <label for='userName' className='form-label'>Name</label>
+                    <input type='text' placeholder='your name' required className='form-input' id='userName' onChange={(e) => setUser({...user, name: e.target.value})} />
+                </div>
+                <div className='form-group'>
+                    <label for='userPassword' className='form-label'>Password</label>
+                    <input type='password' placeholder='password' required className='form-input' id='userPassword' onChange={(e) => setUser({...user, password: e.target.value})} />
+                </div>
+                <div className='form-btn-container'>
+                    <button type='submit' className='form-btn-submit'>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Login;
