fc3f6dc55290c980fa026f40af473689b254cf82
FabianUlises <ulisesfab.car@outlook.com>
2023-04-25 17:23:08 -0700

    Added value to login component inputs

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -13,15 +13,15 @@ const Login = () => {
             <form className='page-form'>
                 <div className='form-group'>
                     <label for='userEmail' className='form-label'>Email</label>
-                    <input type='email' placeholder='your email' required className='form-input' id='userEmail' onChange={(e) => setUser({...user, email: e.target.value})} />
+                    <input value={user.email} type='email' placeholder='your email' required className='form-input' id='userEmail' onChange={(e) => setUser({...user, email: e.target.value})} />
                 </div>
                 <div className='form-group'>
                     <label for='userName' className='form-label'>Name</label>
-                    <input type='text' placeholder='your name' required className='form-input' id='userName' onChange={(e) => setUser({...user, name: e.target.value})} />
+                    <input value={user.name} type='text' placeholder='your name' required className='form-input' id='userName' onChange={(e) => setUser({...user, name: e.target.value})} />
                 </div>
                 <div className='form-group'>
                     <label for='userPassword' className='form-label'>Password</label>
-                    <input type='password' placeholder='password' required className='form-input' id='userPassword' onChange={(e) => setUser({...user, password: e.target.value})} />
+                    <input value={user.password} type='password' placeholder='password' required className='form-input' id='userPassword' onChange={(e) => setUser({...user, password: e.target.value})} />
                 </div>
                 <div className='form-btn-container'>
                     <button type='submit' className='form-btn-submit'>Submit</button>
